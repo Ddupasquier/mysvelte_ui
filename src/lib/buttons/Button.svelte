94a3b7cd8cd3707d7b1014642cbf882b4a8e1d8b
dylandupasquier <dylandupasquier@gmail.com>
2023-02-15 15:07:46 -0800

    bleh

---
+++ src/lib/buttons/Button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+      export let disabled = true
+</script>
+
+<button disabled={disabled} class="button">
+      {#if disabled}
+            Disabled
+      {:else}
+            Enabled
+      {/if}
+</button>
