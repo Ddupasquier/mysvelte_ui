01b47e5bff6a8bcf2ad2323de2ad935e18608bd7
dylandupasquier <dylandupasquier@gmail.com>
2023-02-18 12:31:10 -0800

    -.-

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <svelte:head>
   <title>MySvelteUI</title>
-  <meta name="description" content="Svelte demo app" />
+  <meta name="description" content="Svelte demo app" /> 
 </svelte:head>
 
 <section>
