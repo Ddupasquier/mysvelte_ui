8dfccd7efc1a7281cb6f25962bfac3d8c1136ee5
dylandupasquier <dylandupasquier@gmail.com>
2023-02-19 03:17:37 -0800

    cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
-<script>
-  import { Button, Input } from 'mysvelte-ui';
+<script lang="ts">
+  
 </script>
 
 <svelte:head>
@@ -8,8 +8,6 @@
 </svelte:head>
 
 <section>
-  <Button background="green" size="xlarge" color="black">Test</Button>
-  <Input placeholder="Test" />
   <p>
     Welcome to our Svelte component library! We are dedicated to providing
     developers with a library that is not only functional but also incredibly
