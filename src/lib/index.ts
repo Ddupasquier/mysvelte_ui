e0e753563bdbb2d3dffcb3ba7f1bdd2a612f8568
dylandupasquier <dylandupasquier@gmail.com>
2023-02-22 19:12:27 -0800

    better naming for input default

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,8 @@
 export { default as Button } from './buttons/Button.svelte';
-import Default  from './inputs/Input.svelte';
+
+import DefaultInput from './inputs/Input.svelte';
 import Prefix from './inputs/Prefix.svelte';
 
-export const Input = Object.assign(Default, {
+export const Input = Object.assign(DefaultInput, {
   Prefix,
 });
