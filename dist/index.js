354fa9ee6bf9064593fcc1836acf0429dfadcbcf
Dylan Dupasquier <89223122+Ddupasquier@users.noreply.github.com>
2023-02-24 13:40:56 -0800

    Delete dist directory

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Button } from './buttons/Button.svelte';
-export { default as BubbleButton } from './buttons/BubbleButton.svelte';
-import DefaultInput from './inputs/Input.svelte';
-import Prefix from './inputs/Prefix.svelte';
-export const Input = Object.assign(DefaultInput, {
-    Prefix,
-});
