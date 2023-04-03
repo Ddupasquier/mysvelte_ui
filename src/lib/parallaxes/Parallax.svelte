1e159201bbc176ffdb6b409269775ed4c43b8104
Dylan Dupasquier <dylandupasquier@gmail.com>
2023-04-03 11:02:11 -0700

    update def props and add positioning to parallax

--- src/lib/parallaxes/Parallax.svelte
+++ src/lib/parallaxes/Parallax.svelte
@@ -1,10 +1,10 @@
-<!--  parallax -->
 <script lang="ts">
-    export let image: string;
-    export let height: number;
-    export let width: number;
-    export let alt: string;
+    export let image: string = "";
+    export let alt: string = "";
+    export let height: string = "300px";
+    export let width: string = "100%";
     export let speed: number = 0.5;
+    export let position: [number, number] = [0, 0];
 
     let scroll: number;
 
@@ -25,9 +25,9 @@
         class="parallax"
         {alt}
         {...$$restProps}
-        style:transform={`translate(-50%, -50%) translateY(${
-            scroll * speed
-        }px)`}
+        style:transform={`translate(${position[0] - 50}%, ${
+            position[1] - 50
+        }%) translateY(${scroll * speed}px)`}
     />
 </div>
 
@@ -41,7 +41,6 @@
         position: absolute;
         top: 50%;
         left: 50%;
-        transform: translate(-50%, -50%);
         width: 100%;
         object-fit: cover;
         object-position: center;
---
+++ src/lib/parallaxes/ParallaxVideo.svelte
@@ -0,0 +1,105 @@
+<script lang="ts">
+    export let video: string = "";
+    export let height: string = "300px";
+    export let width: string = "100%";
+    export let speed: number = 0.5;
+    export let position: [number, number] = [0, 0];
+
+    let scroll: number;
+    let videoElement: HTMLVideoElement;
+    let metaLoaded: boolean = false;
+
+    $: {
+        if (speed < -2) {
+            speed = -2;
+        } else if (speed > 2) {
+            speed = 2;
+        }
+    }
+
+    function handleLoadedMetadata(e: Event) {
+        const target = e.target as HTMLVideoElement;
+        if (target) {
+            metaLoaded = true;
+        }
+    }
+
+    function handleClick() {
+        if (videoElement) {
+            if (videoElement.paused) {
+                videoElement.play();
+            } else {
+                videoElement.pause();
+            }
+        }
+    }
+</script>
+
+<svelte:window bind:scrollY={scroll} />
+
+<div class="parallax-wrapper" style="height: {height}; width: {width};">
+    <video
+        bind:this={videoElement}
+        class="parallax"
+        {...$$restProps}
+        style:transform={`translate(${position[0] - 50}%, ${
+            position[1] - 50
+        }%) translateY(${scroll * speed}px)`}
+        loop
+        muted
+        playsinline
+        on:loadedmetadata={handleLoadedMetadata}
+    >
+        <source src={video} type="video/mp4" />
+        <source src={video} type="video/webm" />
+        <source src={video} type="video/ogg" />
+
+        Your browser does not support the video tag.
+    </video>
+    {#if metaLoaded}
+        <svg
+            class="play-button"
+            viewBox="0 0 24 24"
+            width="48"
+            height="48"
+            xmlns="http://www.w3.org/2000/svg"
+            on:click={handleClick}
+            on:keydown={handleClick}
+        >
+            <path d="M8 5v14l11-7z" />
+            <path d="M0 0h24v24H0z" fill="none" />
+        </svg>
+    {/if}
+</div>
+
+<style lang="scss">
+    .parallax-wrapper {
+        overflow: hidden;
+        position: relative;
+    }
+
+    .parallax {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: 100%;
+        object-fit: cover;
+        object-position: center;
+    }
+
+    .play-button {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        fill: white;
+        opacity: 0.5;
+        cursor: pointer;
+        background: black;
+        border-radius: 50rem;
+        transition: opacity 0.2s ease-in-out;
+        &:hover {
+            opacity: 1;
+        }
+    }
+</style>
