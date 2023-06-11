c7fd407b8a60610a10ff1efbbc6f918eb2a64d42
Dylan Dupasquier <dylandupasquier@gmail.com>
2023-06-10 17:33:01 -0700

    updated

--- docs/Avatar_docs.ts
+++ docs/Avatar_docs.ts
@@ -202,41 +202,42 @@ export const avatars: AvatarDisplayData[] = [
     type: 'table',
     table: {
       tableName: 'avatar',
-      rows: [{
-        name: `avatar_image!`,
-        description: 'NEW The URL of the avatar image. Provide the path to the image you want to display as the avatar.',
-        type: '{string}',
-        default: '\"\"',
-        nav: true,
-      },
-      {
-        name: `avatar_alt!`,
-        description: 'The alternative text for the avatar image. This text is displayed when the image cannot be loaded or accessed by screen readers.',
-        type: '{string}',
-        default: '\"\"',
-        nav: false,
-      },
-      {
-        name: `avatar_size`,
-        description: 'The size of the avatar. Choose from \"xsmall\", \"small\", \"medium\", \"large\", or \"xlarge\" to control the dimensions of the avatar.',
-        type: '{"xsmall" | "small" | "medium" | "large" | "xlarge"}',
-        default: '\"medium\"',
-        nav: true,
-      },
-      {
-        name: `avatar_shape`,
-        description: 'The shape of the avatar. Select either \"circ\", \"rounded\", or \"square\" to define the border shape of the avatar.',
-        type: '{"circ" | "rounded" | "square"}',
-        default: '\"rounded\"',
-        nav: true,
-      },
-      {
-        name: `avatar_filter`,
-        description: 'The filter applied to the avatar image. Apply visual effects such as \"gray\" for grayscale, \"sepia\" for a vintage look, or \"invert\" for an inverted color scheme.',
-        type: '{"none" | "gray" | "sepia" | "invert"}',
-        default: '\"none\"',
-        nav: true,
-      }],
+      rows: [
+        {
+          name: 'avatar_image!',
+          description: 'NEW The URL of the avatar image. Provide the path to the image you want to display as the avatar.',
+          type: '{string}',
+          default: '\"\"',
+          nav: true,
+        },
+        {
+          name: 'avatar_alt!',
+          description: 'The alternative text for the avatar image. This text is displayed when the image cannot be loaded or accessed by screen readers.',
+          type: '{string}',
+          default: '\"\"',
+          nav: false,
+        },
+        {
+          name: 'avatar_size',
+          description: 'The size of the avatar. Choose from \"xsmall\", \"small\", \"medium\", \"large\", or \"xlarge\" to control the dimensions of the avatar.',
+          type: '{"xsmall" | "small" | "medium" | "large" | "xlarge"}',
+          default: '\"medium\"',
+          nav: true,
+        },
+        {
+          name: 'avatar_shape',
+          description: 'The shape of the avatar. Select either \"circ\", \"rounded\", or \"square\" to define the border shape of the avatar.',
+          type: '{"circ" | "rounded" | "square"}',
+          default: '\"rounded\"',
+          nav: true,
+        },
+        {
+          name: 'avatar_filter',
+          description: 'The filter applied to the avatar image. Apply visual effects such as \"gray\" for grayscale, \"sepia\" for a vintage look, or \"invert\" for an inverted color scheme.',
+          type: '{"none" | "gray" | "sepia" | "invert"}',
+          default: '\"none\"',
+          nav: true,
+        }],
     },
     examples: [
       {
