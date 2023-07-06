0849ebb49ea8818987dda66b1f0437a75e0c8f49
Dylan Dupasquier <dylandupasquier@gmail.com>
2023-07-06 16:44:12 -0700

    move and add photos for docs

---
+++ docs/assets/index.ts
@@ -0,0 +1,8 @@
+export { default as beach } from './beach.jpg';
+export { default as city } from './city.jpg';
+export { default as desert } from './desert.jpg';
+export { default as flower } from './flower.jpg';
+export { default as boat } from './boat.jpg';
+export { default as cityVideo } from './city.mp4';
+export { default as jungle } from './jungle.jpg';
+export { default as forest } from './forest.jpg';
